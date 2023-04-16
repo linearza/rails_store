15bbf263ebf96dc387bb4923fc73bc8a257eaf06
linearza <vanwijk.mc@gmail.com>
2023-04-16 19:11:57 +0200

    chore: update user model with auth details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  enum role: [:user, :admin]
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  def self.from_token_payload(payload)
+    find payload['sub']
+  end
 end
