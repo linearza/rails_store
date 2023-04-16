66e71a68de9a42e983402258779aba14ea0e2eba
linearza <vanwijk.mc@gmail.com>
2023-04-16 19:11:32 +0200

    chore: adds session controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = JsonWebToken.encode({ user_id: user.id })
+      render json: { token: token }
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+end
