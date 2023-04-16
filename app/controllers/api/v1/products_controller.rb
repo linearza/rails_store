7dadf97601b2e1c279105809a868754491ca7d9b
linearza <vanwijk.mc@gmail.com>
2023-04-16 19:20:01 +0200

    chore: add crud and auth methods to users and products controllers

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :authorize_admin, except: [:index, :show]
+
+  def index
+    products = Product.all
+    render json: products
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      render json: product
+    else
+      render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head :no_content
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :image_url)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authorize_admin, except: [:show, :create, :update]
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      token = JsonWebToken.encode({ user_id: user.id })
+      render json: { token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+    render json: user
+    else
+    render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+    
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
 end
